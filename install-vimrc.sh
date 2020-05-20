0a4eb1628be012443f1217cb38a5ad9158d3352e
zzzzzsad <zzz75326@163.com>
2020-05-20 10:44:18 -0400

    modify install-vimrc.sh

--- install-vimrc.sh
+++ install-vimrc.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-if [ ! -f "~/.vimrc" ]; then
+if [ -f "~/.vimrc" ]; then
 	cp ~/.vimrc ~/.vimrc-bak
 fi
 cp .vim_global_copy_paste.vim ~
