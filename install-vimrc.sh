25beaa32c0b864380ae3591a6e04baad91949c2d
zzzzzsad <zzz75326@163.com>
2020-05-22 10:42:26 +0800

    download vim-plug

--- install-vimrc.sh
+++ install-vimrc.sh
@@ -1,7 +1,15 @@
 #! /bin/bash
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp .vim_global_copy_paste.vim ~
+dir=~/.vim/autoload
+
+if [ ! -d $dir ];then
+	mkdir -p $dir
+fi
+
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp vim-plug_master_plug.vim $dir/plug.vim
+
+cp vim_global_copy_paste.vim ~/.vim_global_copy_paste.vim
 cp ./vimrc ~/.vimrc
 
 
